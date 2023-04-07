051537548fbf02cb70e1e374c1cb16822eabbef9
mahlet <mahletbekele57@gmail.com>
2023-04-07 08:54:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(act = "go to the office"){
+    return (`This Monday, I will ${act}.`);
+}
+
+function wrapAdjective(a1 = "*"){
+    return function (spec = `special`){
+        return `You are ${a1} ${spec} ${a1} !`
+    }
+        
+
+}
+
+
+function wrapAdjective(a1 = "||"){
+    return function(result = "a didicated programmer"){
+        return `You are ${a1}${result}${a1}!`
+    }
+}
+
+//saturdayFun();
